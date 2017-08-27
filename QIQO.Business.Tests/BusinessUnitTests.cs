82daafe2091f44009bba7dd57bf4eb90944e775e
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2017-08-27 15:20:55 -0500

    xunit! meh

--- QIQO.Business.Tests/BusinessUnitTests.cs
+++ QIQO.Business.Tests/BusinessUnitTests.cs
@@ -12,31 +12,31 @@ namespace QIQO.Business.Tests
 {
     public class AccountClientTests
     {
-        public AccountClientTests()
-        {
-            var principal = new GenericPrincipal(
-               new GenericIdentity("Richard Richards"), new string[] { "Administrators", "QIQOOrderEntryAdmin" });
-            Thread.CurrentPrincipal = principal;
-            AppDomain.CurrentDomain.SetThreadPrincipal(principal);
-        }
-
-        //[Fact]
-        //public void AccountClientCreateAccountReturnsInt()
+        //public AccountClientTests()
         //{
-        //    // Arrange
-        //    var account = new Account() { AccountKey = 123 };
-        //    var mockAccountClient = new Mock<IAccountService>();
-
-        //    mockAccountClient.Setup(m => m.CreateAccount(It.IsAny<Account>())).Returns(123);
-
-        //    // SUT
-        //    var sut = new AccountClient(EndpointConfiguration.NetTcpBinding_IAccountService);
-
-        //    // Act
-        //    var ret_val = sut.CreateAccount(account);
-
-        //    // Assert
-        //    Assert.Same(ret_val, 123);
+        //    var principal = new GenericPrincipal(
+        //       new GenericIdentity("Richard Richards"), new string[] { "Administrators", "QIQOOrderEntryAdmin" });
+        //    Thread.CurrentPrincipal = principal;
+        //    AppDomain.CurrentDomain.SetThreadPrincipal(principal);
         //}
+
+        [Fact, AssumeIdentity("QIQOOrderEntryAdmin")]
+        public void AccountClientCreateAccountReturnsInt()
+        {
+            // Arrange
+            var account = new Account() { AccountKey = 123 };
+            var mockAccountClient = new Mock<IAccountService>();
+
+            mockAccountClient.Setup(m => m.CreateAccount(It.IsAny<Account>())).Returns(123);
+
+            // SUT
+            var sut = new AccountClient(EndpointConfiguration.NetTcpBinding_IAccountService);
+
+            // Act
+            var ret_val = sut.CreateAccount(account);
+
+            // Assert
+            Assert.Same(ret_val, 123);
+        }
     }
 }
